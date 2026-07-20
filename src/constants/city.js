96a0cb045ab25f19ae74c1971ac091114b4ba423
anvarimajd <anvari_majd@proton.me>
2026-07-20 21:44:38 +0330

    make constants

---
+++ src/constants/city.js
@@ -0,0 +1,6 @@
+export const CITIES = [
+  { label: "تهران ، ایران", value: "Tehran" },
+  { label: "لندن ، انگلیس", value: "London" },
+  { label: "سیدنی ، استرالیا", value: "Sydney" },
+  { label: "توکیو ، ژاپن", value: "Tokyo" },
+];
